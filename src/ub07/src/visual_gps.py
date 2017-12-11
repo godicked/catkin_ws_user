d6afbdde1e2fa14ded3c77888b8c704931a0b098
David Gödicke <godicked@gmail.com>
2017-12-11 12:13:14 +0100

    fix
    Former-commit-id: c1d0c9ec0e461fddfa632395580d450c99b72612

--- src/ub07/src/visual_gps.py
+++ src/ub07/src/visual_gps.py
@@ -33,59 +33,46 @@ class image_converter:
     except CvBridgeError as e:
       print(e)
 
-    ### BUILD RGB ###
-    b,g,r = cv2.split(cv_image)
-    r_blur = cv2.GaussianBlur(r,(5,5),0)
-    g_blur = cv2.GaussianBlur(g,(5,5),0)
-    b_blur = cv2.GaussianBlur(b,(5,5),0)
+    # Blur image to remove color noise
+    cv_image = cv2.GaussianBlur(cv_image, (5,5), 0)
+
+    # cv2.imshow("img", cv_image)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        return
 
 
-    yuv = cv2.cvtColor(cv_image,cv2.COLOR_BGR2YUV);
-    y,u,v = cv2.split(yuv)
-    v_blur = cv2.GaussianBlur(v,(5,5),0)
-
-
-    # define range of balloons in RGB
-    lower_rgb = np.array([[10, 46, 0], #green
+    # define range of lamps in RGB
+    lower_rgb = np.array([[30, 110, 50], #green
                          [220, 40 , 40], #red
                          [70, 40, 240], #blue
-                         [230, 100, 230]]) #purple
-    lower_rgb = np.fliplr(lower_rgb) # flip to b, g, r
+                         [190, 100, 190]]) #purple
+    lower_rgb = np.fliplr(lower_rgb) # flip to b,g,r for cv
 
-    upper_rgb = np.array([[33,57, 12], # green
+    upper_rgb = np.array([[70, 140, 70], # green
                          [255, 73, 70], #red
                          [90, 60, 255], #blue 
                          [255, 150, 255]]) #purple
-    upper_rgb = np.fliplr(upper_rgb) # flip to b, g, r
+    upper_rgb = np.fliplr(upper_rgb) # flip to b,g,r for cv
 
-    lower_v = np.array([[166],
-                       [124],
-                       [50],
-                       [83]])
-    upper_v = np.array([[178],
-                       [138],
-                       [70],
-                       [110]])
-
-    image = cv_image
-    #images = {}
-    balloons_seen = [0]*4
-    balloon_pos_im = [0]*4
+    lamps_seen = [0]*4
+    lamp_image = [0]*4
 
     for k in range(4):
+        # find lamp k in cv_image
         mask_rgb = cv2.inRange(cv_image, lower_rgb[k], upper_rgb[k])
-        mask_v   = cv2.inRange(v  , lower_v[k], upper_v[k])
-        just_balloon = cv2.bitwise_and(mask_rgb, mask_v)
-        #images[k] = cv2.bitwise_and(mask_rgb, mask_v)
         indices = np.nonzero(mask_rgb)
         
-        balloon_pos_im[k] = [ np.mean(indices[0]), np.mean(indices[1]) ]
-        if not isnan(balloon_pos_im[k][0]): # check, if balloon appeared in picture
-            balloons_seen[k] =1
-            image = cv2.circle(image, tuple([ int(balloon_pos_im[k][1]) ,int( balloon_pos_im[k][0]) ]), 6, upper_rgb[k], 2)
+        # save lamp coordinate
+        lamp_image[k] = [ np.mean(indices[1]), np.mean(indices[0]) ]
+
+        # check if lamp is found
+        if not isnan(lamp_image[k][0]):
+            lamps_seen[k] =1
+            #image = cv2.circle(image, tuple([ int(lamp_image[k][1]) ,int( lamp_image[k][0]) ]), 6, upper_rgb[k], 2)
 
 
-    balloon_pos_rw = [
+    lamp_world = [
         [2.29, 1.14 ], #green lamp
         [3.55, 3.03 ], #red lamp
         [4.18, 1.77 ], #blue lamp
@@ -93,19 +80,16 @@ class image_converter:
 
     #centers of lamps in image
     center_im = np.mean(
-        np.array([ balloon_pos_im[k] for k in range(4) if balloons_seen[k] ]),
+        np.array([ lamp_image[k] for k in range(4) if lamps_seen[k] ]),
         axis=0
         )
 
-    print 'center image', center_im
-
     #center of lamps in real world
     center_rw = np.mean(
-        np.array([ balloon_pos_rw[k] for k in range(4) if balloons_seen[k] ]),
+        np.array([ lamp_world[k] for k in range(4) if lamps_seen[k] ]),
         axis=0)
 #    print('center  rw ', center_rw)
 
-    print "center rw", center_rw
 
     # calculate rotation matrix
     scaling = []
@@ -113,35 +97,40 @@ class image_converter:
     sum_x=0
     sum_y=0
     for k in range(4):
-        if balloons_seen[k]:
-            scaling.append( np.sqrt(  ((balloon_pos_rw[k][0]-center_rw[0])**2+( balloon_pos_rw[k][1]-center_rw[1] )**2) / float(((balloon_pos_im[k][0]-center_im[0])**2+( balloon_pos_im[k][1]-center_im[1] )**2)) ) )
+        if lamps_seen[k]:
+            scaling.append( np.sqrt(  ((lamp_world[k][0]-center_rw[0])**2+( lamp_world[k][1]-center_rw[1] )**2) / float(((lamp_image[k][0]-center_im[0])**2+( lamp_image[k][1]-center_im[1] )**2)) ) )
 
-            atan_1 = atan2( (balloon_pos_rw[k][1] - center_rw[1] ), (balloon_pos_rw[k][0] - center_rw[0]) )
+            atan_world = atan2( (lamp_world[k][1] - center_rw[1] ), (lamp_world[k][0] - center_rw[0]) )
 
-            atan_2 = atan2( (balloon_pos_im[k][1] - center_im[1]), (balloon_pos_im[k][0] - center_im[0]) )
+            atan_image = atan2( (lamp_image[k][1] - center_im[1]), (lamp_image[k][0] - center_im[0]) )
 
 
-            print 'atan1', atan_1
-            print 'atan2', atan_2
+            # print 'atan1', atan_1
+            # print 'atan2', atan_2
 
-            rotation_angle.append( atan_1 - atan_2 )
-            b_x=balloon_pos_im[k][0] - center_im[0]
-            b_y=balloon_pos_im[k][1] - center_im[1]
-            w_x=balloon_pos_rw[k][0] - center_rw[0]
-            w_y=balloon_pos_rw[k][1] - center_rw[1]
+            #rotation between world and image is rotation between both points
+            rotation_angle.append( atan_world - atan_image )
+
+            b_x=lamp_image[k][0] - center_im[0]
+            b_y=lamp_image[k][1] - center_im[1]
+            w_x=lamp_world[k][0] - center_rw[0]
+            w_y=lamp_world[k][1] - center_rw[1]
             sum_x+=(b_x*w_y-b_y*w_x)
             sum_y+=b_x*w_x+b_y*w_y
+
             if rotation_angle[-1] > 2*pi:
                 rotation_angle[-1] -= 2*pi
             if rotation_angle[-1] < 0:
                 rotation_angle[-1] += 2*pi
 
-    print(balloons_seen)
+    print(lamps_seen)
     scaling_mean = np.mean( scaling)
     rotation_angle_mean = np.mean( rotation_angle[1:], axis = 0 )
+    print 'angle_mean 1', rotation_angle_mean
     rotation_angle_mean = atan2(sum_x,sum_y)
+    print 'angle_mean 2', rotation_angle_mean
 
-    print( 'angle: ' + str(rotation_angle))
+    # print( 'angle: ' + str(rotation_angle))
     print( 'scaling: ' + str(scaling_mean))
 
     R = scaling_mean * np.array([
@@ -150,12 +139,14 @@ class image_converter:
         ])
 
     # current_pos_im = [226, 317]
-    current_pos_im = [240, 320]
-    current_pos_rw = np.dot(R, current_pos_im - center_im) + center_rw
+    pos_image = [1280 / 2.0, 720 / 2.0]
+    pos_world = np.dot(R, pos_image - center_im) + center_rw
+
+    print 'position in world', pos_world
 
 
     try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(image, "bgr8"))
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
     except CvBridgeError as e:
       print(e)
       
@@ -163,12 +154,12 @@ class image_converter:
     odom1 = Odometry()
     odom1.header.frame_id  = 'map'
     odom1.header.seq = data.header.seq
-    odom1.pose.pose.position.x = current_pos_rw[0]
-    odom1.pose.pose.position.y = -current_pos_rw[1]
+    odom1.pose.pose.position.x = pos_world[0]
+    odom1.pose.pose.position.y = -pos_world[1]
 
     odom1.pose.pose.orientation = Quaternion( *tf.transformations.quaternion_from_euler(0, 0, pi-1.0*rotation_angle_mean ) )
 
-    if sum(balloons_seen) >= 3:
+    if sum(lamps_seen) >= 3:
     	self.gps_pub.publish( odom1 )
 
 def main(args):
